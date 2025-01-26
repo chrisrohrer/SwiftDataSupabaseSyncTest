3b475c7bf26f8c0fa9763293fe82e4148d4bb891
Christoph Rohrer <christoph@rohrers.de>
2025-01-26 17:05:18 +0100

    Initial Commit

---
+++ SwiftDataSupabaseSyncTest/SwiftDataSupabaseSyncTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSupabaseSyncTestApp.swift
+//  SwiftDataSupabaseSyncTest
+//
+//  Created by Christoph Rohrer on 26.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataSupabaseSyncTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
